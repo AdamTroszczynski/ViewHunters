61bea517e21dcb048157147a6c7332c84ae00c50
Amanowsky <adamtroszczynski@outlook.com>
2024-04-03 14:32:17 +0200

    init commit btn

---
+++ client/src/components/buttons/ActionButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-button class="actionButton">
+    <slot name="icon"></slot>
+    <ion-label><slot name="label"></slot></ion-label>
+  </ion-button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  isGreen: {
+    type: Boolean,
+    default: false,
+  },
+  isOrange: {
+    type: Boolean,
+    default: false,
+  },
+  isBlackOrange: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.actionButton {
+  width: 100%;
+  padding: 0.5rem;
+}
+</style>
