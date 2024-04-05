abeb82766b559b5d14d9f7ba2d5940841607a2d6
Amanowsky <adamtroszczynski@outlook.com>
2024-04-05 22:40:22 +0200

    labels added

---
+++ client/src/components/labels/TitleLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <IonText class="titleLabel">
+    <h1 class="titleLabel__header"><slot></slot></h1>
+  </IonText>
+</template>
+
+<script setup lang="ts">
+import { IonText } from '@ionic/vue';
+</script>
+
+<style lang="scss" scoped>
+.titleLabel {
+  &__header {
+    text-align: center;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 1.25rem;
+    color: #37414a;
+  }
+}
+</style>
