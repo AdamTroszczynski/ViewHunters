305c19ebf2a30fb6d57ebf4a2f06082559d2dcbf
Amanowsky <adamtroszczynski@outlook.com>
2024-04-05 23:34:39 +0200

    AchievCard added

---
+++ client/src/components/cards/AchievCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <IonCard class="achievCard">
+    <AchievementIcon class="achievCard__icon" />
+    <IonCardHeader class="achievCard__header">
+      <IonCardTitle class="achievCard__title"> <slot></slot> </IonCardTitle>
+    </IonCardHeader>
+  </IonCard>
+</template>
+
+<script setup lang="ts">
+import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/vue';
+import AchievementIcon from '@/components/icons/AchievementIcon.vue';
+</script>
+
+<style lang="scss" scoped>
+.achievCard {
+  --background: $white;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  padding-top: 1.4rem;
+  border-radius: 8px;
+
+  &__header {
+    padding-top: 0.5rem;
+  }
+
+  &__title {
+    font-family: $poppins;
+    font-size: 0.75rem;
+    font-weight: 500;
+    color: #37414a;
+  }
+}
+</style>
