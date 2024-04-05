e98246d5f614415a0cfe9b8b41a28ef035610392
Amanowsky <adamtroszczynski@outlook.com>
2024-04-05 18:08:01 +0200

    ActionBtn added, icons fix

--- client/src/components/buttons/ActionButton.vue
+++ client/src/components/buttons/ActionButton.vue
@@ -1,12 +1,30 @@
 <template>
-  <ion-button class="actionButton">
-    <slot name="icon"></slot>
-    <ion-label><slot name="label"></slot></ion-label>
-  </ion-button>
+  <IonButton
+    class="actionButton"
+    :class="[
+      isGreen ? 'actionButton--is-green' : '',
+      isOrange ? 'actionButton--is-orange' : '',
+      isBlackOrange ? 'actionButton--is-blackOrange' : '',
+    ]"
+  >
+    <div class="actionButton__content">
+      <div v-if="icon !== ''" class="actionButton__icon">
+        <component :is="setIcon"></component>
+      </div>
+      <div class="actionButton__text"><slot></slot></div>
+    </div>
+  </IonButton>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { IonButton } from '@ionic/vue';
+import ArrowIcon from '@/components/icons/usesIcons/ArrowIcon.vue';
+import CheckIcon from '@/components/icons/usesIcons/CheckIcon.vue';
+import CodeIcon from '@/components/icons/usesIcons/CodeIcon.vue';
+import LocationIcon from '@/components/icons/usesIcons/LocationIcon.vue';
+import { computed, type Component } from 'vue';
+
+const props = defineProps({
   isGreen: {
     type: Boolean,
     default: false,
@@ -19,12 +37,74 @@ defineProps({
     type: Boolean,
     default: false,
   },
+  icon: {
+    type: String,
+    default: '',
+    validator(val: string): boolean {
+      return ['arrow', 'check', 'code', 'location', ''].includes(val);
+    },
+  },
+});
+
+const setIcon = computed<Component | null>(() => {
+  switch (props.icon) {
+    case 'arrow':
+      return ArrowIcon;
+    case 'check':
+      return CheckIcon;
+    case 'code':
+      return CodeIcon;
+    case 'location':
+      return LocationIcon;
+    default:
+      return null;
+  }
 });
 </script>
 
 <style lang="scss" scoped>
 .actionButton {
-  width: 100%;
-  padding: 0.5rem;
+  font-family: $poppins;
+  --border-radius: 8px;
+  --width: 100%;
+  height: 25px;
+  min-height: 0;
+  text-transform: none;
+  --background: #293b4f;
+  --padding-start: 1.125rem;
+  --padding-end: 1.125rem;
+  --padding-top: 0.3125rem;
+  --padding-bottom: 0.3125rem;
+
+  &__content {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.625rem;
+  }
+  &__icon {
+    height: 12px;
+    width: 12px;
+  }
+  &__text {
+    font-family: $poppins;
+    font-size: 0.7rem;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+  &--is-green {
+    --background: #13dac0;
+  }
+  &--is-orange {
+    --background: #f48516;
+  }
+  &--is-blackOrange {
+    --background: none;
+    color: #f48516;
+    --border-width: 2px;
+    --border-style: solid;
+    --border-color: #f48516;
+  }
 }
 </style>
