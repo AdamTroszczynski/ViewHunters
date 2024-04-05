204d60deb6f995ef8108a3428d262d622b2557e9
Amanowsky <adamtroszczynski@outlook.com>
2024-04-05 22:22:48 +0200

    DetailsLabel added

---
+++ client/src/components/labels/DetailLabel.vue
@@ -0,0 +1,26 @@
+<template>
+  <IonChip class="detailLabel">HGaloooo</IonChip>
+</template>
+
+<script setup lang="ts">
+import { IonChip } from '@ionic/vue';
+</script>
+
+<style lang="scss" scoped>
+.detailLabel {
+  --width: 100%;
+  min-height: 0;
+  height: 17px;
+  padding-left: 1.125rem;
+  padding-right: 1.125rem;
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  border-radius: 6px;
+  background: white;
+  text-align: center;
+  font-family: $poppins;
+  font-weight: 500;
+  font-size: 0.625rem;
+  color: #293b4f;
+}
+</style>
