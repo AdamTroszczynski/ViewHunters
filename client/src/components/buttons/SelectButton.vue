c183091348689eefcbcce25c758c86e1977a68a0
Amanowsky <adamtroszczynski@outlook.com>
2024-04-17 01:06:53 +0200

    SelectBtn addec, PlaceFilter logic added, change types in placeStore

---
+++ client/src/components/buttons/SelectButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <IonSelect
+    :aria-label="label"
+    :value="startingValue"
+    interface="popover"
+    class="selectButton"
+    :class="isGreen ? 'selectButton--is-green' : ''"
+  >
+    <IonSelectOption v-for="option in options" :key="option" :value="option">{{
+      option
+    }}</IonSelectOption>
+  </IonSelect>
+</template>
+
+<script setup lang="ts">
+import { IonSelect, IonSelectOption } from '@ionic/vue';
+defineProps({
+  options: {
+    type: Array<string>,
+    required: true,
+  },
+  startingValue: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  isGreen: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.selectButton {
+  --border-radius: 8px;
+  --background: #f48516;
+  --padding-top: 0.3125rem;
+  --padding-bottom: 0.3125rem;
+  --highlight-color-focused: none;
+  height: 25px;
+  min-height: 0;
+  min-width: 85px;
+  text-transform: none;
+  font-family: $poppins;
+  color: white;
+  font-size: 0.63rem;
+  font-weight: bold;
+
+  &--is-green {
+    --background: #13dac0;
+  }
+}
+ion-select::part(icon) {
+  display: none;
+}
+
+ion-select::part(text) {
+  min-width: 85px;
+  text-align: center;
+}
+</style>
--- client/src/stores/placeStore.ts
+++ client/src/stores/placeStore.ts
@@ -2,7 +2,7 @@ import { ref, type Ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const usePlaceStore = defineStore('placeStore', () => {
-  const selectedCategory: Ref<string> = ref('Building');
+  const selectedCategory: Ref<string> = ref('Buildings');
   const selectedDistanse: Ref<number> = ref(5);
 
   return { selectedCategory, selectedDistanse };
