aebaa8e686cc99ffc0ababe78888906113fc7b4b
Amanowsky <adamtroszczynski@outlook.com>
2024-04-18 21:38:34 +0200

    Place type file added

---
+++ client/src/types/Place.ts
@@ -0,0 +1,11 @@
+export type Place = {
+  id: number;
+  name: string;
+  geoWidth: number;
+  geoHeight: number;
+  photo: string[];
+  category: string;
+  description: string;
+  code: string;
+  isDiscovered: boolean;
+};
