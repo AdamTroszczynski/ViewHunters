181ec5cf9cbdf0320c075c740d5ff0fb729ab92f
Amanowsky <adamtroszczynski@outlook.com>
2024-04-18 23:24:10 +0200

    add route to placeDetails

---
+++ client/src/views/PlaceDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="placeDetails">
+    <HeroBanner />
+    <div class="placeDetails__container">
+      <div>
+        <div class="placeDetails__photo"></div>
+        <div class="placeDetails__title"></div>
+        <p class="placeDetails__text"></p>
+      </div>
+      <ActionButton :icon="'arrow'">Back</ActionButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeroBanner from '@/components/common/HeroBanner.vue';
+import ActionButton from '@/components/buttons/ActionButton.vue';
+</script>
