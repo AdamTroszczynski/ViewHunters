de55e0e351b742dc87945adcdddedaa5bdc443b7
Amanowsky <adamtroszczynski@outlook.com>
2024-04-19 13:29:20 +0200

    swiper gallery added

---
+++ client/src/widgets/PhotoGallery.vue
@@ -0,0 +1,57 @@
+<template>
+  <swiper-container
+    :slides-per-view="1"
+    :space-between="15"
+    class="photoGallery"
+  >
+    <swiper-slide v-for="photo in photos" :key="photo"
+      ><div
+        :style="`background-image: url(${photo})`"
+        class="photoGallery__photo"
+      ></div
+    ></swiper-slide>
+  </swiper-container>
+</template>
+
+<script setup lang="ts">
+import { register } from 'swiper/element/bundle';
+
+register();
+
+defineProps({
+  photos: {
+    type: Array<string>,
+    required: true,
+  },
+});
+</script>
+<style lang="scss" scoped>
+.photoGallery {
+  width: 100%;
+  height: 183px;
+  position: relative;
+
+  &__photo {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    border-radius: 8px;
+  }
+
+  &__iconL {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 40%;
+    left: 5%;
+  }
+
+  &__iconR {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 40%;
+    right: 5%;
+  }
+}
+</style>
