83b15b87367ddb55eab736d36481a9e516183d12
Amanowsky <adamtroszczynski@outlook.com>
2024-04-19 13:56:51 +0200

    add swiper to customElement options

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,20 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    legacy()
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all components starting with swiper as custom elements
+          isCustomElement: (tag) => tag.startsWith('swiper'),
+        },
+      },
+    }),
+    legacy(),
   ],
   resolve: {
     alias: {
