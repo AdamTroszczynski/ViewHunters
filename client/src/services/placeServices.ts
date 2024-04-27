c6593eced24aff99a8dbf0c486a7e9d2aa36fa22
Adamtroszczynski <adamtroszczynski@outlook.com>
2024-04-27 16:43:15 +0200

    axiosClient added, placeServices added

---
+++ client/src/services/placeServices.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import axiosClient from "@/utils/axiosClient";
+import { type Place } from "@/types/Place";
+
+export const getSinglePlace = async (id: number): Promise<Place> => {
+    try {
+        const response = await axiosClient.get(`/place/${id}`);
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('getSinglePlace error');
+    }
+}
+
+export const getPLaces = async (geoWidth: number, geoHeight: number): Promise<Place[]> => {
+    try {
+        const response = await axiosClient.get(`/place?geoWidth=${geoWidth}&geoHeight=${geoHeight}`);
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('getPlaces error');
+    }
+}
---
+++ client/src/services/userServices.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+import axiosClient from "@/utils/axiosClient";
+
