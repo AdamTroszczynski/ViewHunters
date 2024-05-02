39b3c216cea20051123cb77e608b8fc643fcdafd
Amanowsky <adamtroszczynski@outlook.com>
2024-05-02 19:58:37 +0200

    merge fix

--- client/src/widgets/NearbyPlaces.vue
+++ client/src/widgets/NearbyPlaces.vue
@@ -48,15 +48,6 @@ const choosePath = (id: number, isDiscovered: boolean) => {
   }
 };
 
-/** Choose where redirect */
-const choosePath = (id: number, isDiscovered: boolean) => {
-  if (isDiscovered) {
-    router.navigate(`/placeDetail/${id}`, 'forward', 'push', forwardAnimation);
-  } else {
-    router.navigate(`/placeUnlock/${id}`, 'forward', 'push', forwardAnimation);
-  }
-};
-
 /** Filter places from store
  * @return {Places[]}
  */
