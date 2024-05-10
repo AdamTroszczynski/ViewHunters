9593825b2c98405c99ef888a568bc875c2273f69
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-05-10 17:14:27 +0200

    Created unlock new place endpoint

--- server/src/router/placeRouter.ts
+++ server/src/router/placeRouter.ts
@@ -1,13 +1,19 @@
 import express from 'express';
-import { getNearestPlacesAction, getSinglePlaceAction } from '@/controller/placeController';
+import { getNearestPlacesAction, getSinglePlaceAction, unlockPlaceAction } from '@/controller/placeController';
 import { verifyToken } from '@/middleware/auth';
 import { validRequest } from '@/middleware/validators/commonValidators';
-import { getNearestPlacesValidators, getSinglePlaceValidators } from '@/middleware/validators/placeValidators';
+import {
+  getNearestPlacesValidators,
+  getSinglePlaceValidators,
+  getUnlockPlaceValidators,
+} from '@/middleware/validators/placeValidators';
 
 const placeRouter = express.Router();
 
 placeRouter.get('/places', [verifyToken, ...getNearestPlacesValidators, validRequest], getNearestPlacesAction);
 
 placeRouter.get('/places/:id', [verifyToken, ...getSinglePlaceValidators, validRequest], getSinglePlaceAction);
+
+placeRouter.post('/places/unlock', [verifyToken, ...getUnlockPlaceValidators, validRequest], unlockPlaceAction);
 
 export default placeRouter;
