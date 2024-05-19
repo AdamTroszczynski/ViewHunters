f31893aa8b9e89e3c92e8885fa81edf543513f0d
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-05-19 16:57:52 +0200

    removed token requirement from image endpoint in asset controller

--- server/src/router/assetRouter.ts
+++ server/src/router/assetRouter.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { getImageAction } from '@/controller/assetsController';
-import { verifyToken } from '@/middleware/auth';
 import { getImageValidators } from '@/middleware/validators/assetValidators';
 import { validRequest } from '@/middleware/validators/commonValidators';
 
 const assetRouter = express.Router();
 
-assetRouter.get('/image/:placeId/:imageName', [verifyToken, ...getImageValidators, validRequest], getImageAction);
+assetRouter.get('/image/:placeId/:imageName', [...getImageValidators, validRequest], getImageAction);
 
 export default assetRouter;
